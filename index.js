89c2683f58016890f0aae552047831ad3570c35f
seenso <>
2021-10-04 11:20:42 -0800

    init cats arr and created functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {}
+
+function destructivelyPrependCat(name) {}
+
+function destructivelyRemoveLastCat() {}
+
+function destructivelyRemoveFirstCat() {}
+
+function appendCat(name) {}
+    
+function prependCat(name) {}
+
+function removeLastCat() {}
+
+function removeFirstCat() {}
